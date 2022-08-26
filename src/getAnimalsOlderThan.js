d763ccb9b2341f1e5465e763fec55938e9cfb63d
Gabriel Galera <gabgalera@hotmail.com>
2022-08-26 16:22:42 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const a = data.species.filter((entry) => entry.name === animal);
+  return a[0].residents.every((entry) => entry.age > age);
 }
 
+console.log(getAnimalsOlderThan('lions', 5));
 module.exports = getAnimalsOlderThan;
