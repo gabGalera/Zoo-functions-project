c95929b889e7b67b6f39c176db6e3e1f0ab5ecc6
Gabriel Galera <gabgalera@hotmail.com>
2022-08-26 20:30:09 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,32 @@
+/* eslint-disable max-lines-per-function */
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const resultado = data.employees
+    .map((entry) => entry.managers)
+    .map((entry) => entry.some((elemento) => elemento === id));
+  // const a = data.employees.some((entry) => entry.managers === id);
+  return resultado.some((entry) => entry === true);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  try {
+    return data.employees
+      .reduce((acc, curr) => {
+        curr.managers.map((entry) => (entry === managerId ? acc.push(curr) : acc));
+        return acc;
+      }, []).map((entry) => `${entry.firstName} ${entry.lastName}`);
+  } catch (error) {
+    return error.message;
+  }
 }
 
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+
+console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 module.exports = { isManager, getRelatedEmployees };
