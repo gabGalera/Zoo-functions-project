6e4cdb3f6507b665ea2e7978f498eb54ecaeddfd
Gabriel Galera <gabgalera@hotmail.com>
2022-08-29 13:45:12 -0300

    Requisito 9 refatorado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,45 +1,24 @@
-/* eslint-disable max-lines-per-function */
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
+const op = Object.values(data.hours);
+
+function getSchedule(st) {
   // seu código aqui
-
-  const animals = data.species
-    .map((entry) => entry.name);
-
-  const weekDays = Object.keys(data.hours);
-  const openClose = Object.values(data.hours);
   const obj = {};
-  const exhibition = data.species.map((entry) => ({
-    ava: entry.availability,
-    nome: entry.name,
-  }));
-
-  weekDays
-    .forEach((entry, index) => {
-      obj[entry] = {
-        officeHour: `Open from ${openClose[index].open}am until ${openClose[index].close}pm`,
-        exhibition: exhibition
-          .filter((elemento) => elemento.ava.includes(entry))
-          .map((elemento) => elemento.nome) };
-    });
-
+  Object.keys(data.hours).forEach((e, i) => {
+    obj[e] = { officeHour: `Open from ${op[i].open}am until ${op[i].close}pm`,
+      exhibition: data.species.map((e) => ({
+        ava: e.availability,
+        nome: e.name })).filter((el) => el.ava.includes(e)).map((el) => el.nome) };
+  });
   obj.Monday.exhibition = 'The zoo will be closed!';
   obj.Monday.officeHour = 'CLOSED';
-
-  if (scheduleTarget === 'Monday') {
-    return { Monday: obj.Monday };
+  if (st === 'Monday') { return { Monday: obj.Monday }; }
+  if (Object.keys(data.hours).includes(st)) {
+    return { [st]: Object.entries(obj).filter((entry) => entry[0] === st)[0][1] };
   }
-
-  if (weekDays.includes(scheduleTarget)) {
-    return {
-      [scheduleTarget]: Object.entries(obj).filter((entry) => entry[0] === scheduleTarget)[0][1],
-    };
-  }
-
-  if (animals.includes(scheduleTarget)) {
-    return data.species
-      .filter((entry) => entry.name === scheduleTarget)[0].availability;
+  if (data.species.map((entry) => entry.name).includes(st)) {
+    return data.species.filter((entry) => entry.name === st)[0].availability;
   }
   return obj;
 }
