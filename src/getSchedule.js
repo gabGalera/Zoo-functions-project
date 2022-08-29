78e83c453614472c60e8c00bec04a1282c28d557
Gabriel Galera <gabgalera@hotmail.com>
2022-08-29 10:52:43 -0300

    Requisito 8 outra vez

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  return data.species
+    .filter((entry) => entry.name === scheduleTarget)[0].availability;
 }
 
+console.log(getSchedule('tigers'));
 module.exports = getSchedule;
