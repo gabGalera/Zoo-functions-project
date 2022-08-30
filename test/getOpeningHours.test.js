222ef9f83aa6b0a64e15922e29b08c58c6594fc8
Gabriel Galera <gabgalera@hotmail.com>
2022-08-30 09:54:17 -0300

    Requisito 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -15,7 +15,21 @@ describe('Testes da função getOpeningHours', () => {
   it('Testa se está fechado na segunda.', () => {
     expect(getOpeningHours('Monday', '09:00-AM')).toMatch('The zoo is closed');
   });
-  it('Testa se abre ma terça.', () => {
+
+  it('Testa se abre na terça.', () => {
     expect(getOpeningHours('Tuesday', '09:00-AM')).toMatch('The zoo is open');
+  });
+
+  it('Testa se fecha na quarta à noite.', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toMatch('The zoo is closed');
+  });
+  it('Testa se lança exceção quando o dia é escrito errado.', () => {
+    expect(() => { getOpeningHours('Thu', '09:00-AM'); }).toThrow('The day must be valid. Example: Monday');
+  });
+  it('Testa se lança exceção quando a abreviação é escrita errado.', () => {
+    expect(() => { getOpeningHours('Thursday', '09:00-ZM'); }).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('Testa se lança exceção quando o horário é escrito errado.', () => {
+    expect(() => { getOpeningHours('Thursday', 'C9:00-AM'); }).toThrow('The hour should represent a number');
   });
 });
