12fdcc94678176b1c7db50ca3390a43c73de702c
Gabriel Galera <gabgalera@hotmail.com>
2022-08-30 09:43:52 -0300

    Requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,21 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Testa se na ausência de dia e hora, retorna  a tabela de horários', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  it('Testa se está fechado na segunda.', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toMatch('The zoo is closed');
+  });
+  it('Testa se abre ma terça.', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toMatch('The zoo is open');
+  });
+});
